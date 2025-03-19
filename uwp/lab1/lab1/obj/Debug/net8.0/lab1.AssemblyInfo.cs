f8493a3a9fbd81dadefafb9d06b18fad57a8b8bf
NorteX <contact@nortexdev.com>
2025-03-19 23:19:21 +0100

    a

--- uwp/lab1/lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ uwp/lab1/lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0fa3b47a99c8e153ae9804b7b9d021ae82039e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6419f23fa8c71fc7b0bdac923d755db0e901138a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
